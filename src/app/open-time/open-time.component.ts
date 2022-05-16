24eb43ced9626f946aff2d2b377262024ce73a33
shdwwlkr05 <shdwwlkr05@gmail.com>
2022-05-16 19:58:32 -0400

    updated opentime page title

--- src/app/open-time/open-time.component.ts
+++ src/app/open-time/open-time.component.ts
@@ -19,7 +19,7 @@ export class OpenTimeComponent implements OnInit {
   faArrowCircleUp = faArrowCircleUp
   faArrowCircleDown = faArrowCircleDown
   faTrash = faTrash
-  title: string = 'Open Time for May 2022'
+  title: string = 'Open Time for June 2022'
   bids = []
   numberOfBids: number = 0
   received_ids = []
